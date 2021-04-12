f70cdcb4eef4a6b5084b2db634e49122eebc3377
Denis Pankov <idenispankov@gmail.com>
2021-04-12 16:56:21 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { useState, useEffect } from 'react';
+
 function App() {
-  return <div className='page'>Welcome to app</div>;
+  const [boxes, setBoxes] = useState([
+    { bg: '#' + ((Math.random() * 0xffffff) << 0).toString(16) },
+  ]);
+
+  useEffect(() => {
+    if (boxes.length <= 500) {
+      const interval = setInterval(() => {
+        setBoxes([
+          ...boxes,
+          { bg: '#' + ((Math.random() * 0xffffff) << 0).toString(16) },
+        ]);
+      }, 100);
+      return () => clearInterval(interval);
+    }
+  }, [boxes]);
+
+  return (
+    <div className='container'>
+      {boxes.map((box, index) => (
+        <div key={index} className='box' style={{ backgroundColor: box.bg }}>
+          <p className='box__text'>{index}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
